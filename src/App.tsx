b07f3e23e49002726d832108d2976ac8c4c819bc
Owner <test>
2023-03-14 11:53:12 +0300

    feat: add router

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route } from "react-router-dom"
+import MainPage from './pages/MainPage'
+import AboutPage from './pages/AboutPage'
+import ErrorPage from './pages/ErrorPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <MainPage/> } />
+        <Route path="about" element={ <AboutPage/> } />
+        <Route path="*" element={ <ErrorPage/> } />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
