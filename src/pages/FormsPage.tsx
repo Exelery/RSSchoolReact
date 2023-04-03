7126fb0074976481e3a56c0864b7c2e530e3955e
Owner <test>
2023-04-03 20:05:26 +0300

    feat: add notification again

--- src/pages/FormsPage.tsx
+++ src/pages/FormsPage.tsx
@@ -4,13 +4,24 @@ import '@/styles/forms.scss';
 import { IItem } from '@/utils/types';
 import CardsList from '@/components/CardsList';
 import FormComponent from '@/components/form/FormComponent';
+import Notification from '../components/form/Notification';
 
 export default function FormsPage() {
   const [items, setItems] = useState<IItem[]>([]);
+  const [notificationVisible, setNotificationVisible] = useState(false);
   const [id, setId] = useState(0);
   function addCard(item: IItem) {
     setItems((prev) => [...prev, { ...item, id: id }]);
     setId((prev) => ++prev);
+    showNotification();
+  }
+  let timer: ReturnType<typeof setTimeout> | undefined = undefined;
+  function showNotification() {
+    if (timer) clearTimeout(timer);
+    setNotificationVisible(true);
+    timer = setTimeout(() => {
+      setNotificationVisible(false);
+    }, 3000);
   }
 
   return (
@@ -19,6 +30,7 @@ export default function FormsPage() {
       <div className="Forms flex md:flex-wrap m-8 gap-8">
         <FormComponent addCard={addCard} />
         <CardsList items={items} />
+        {notificationVisible ? <Notification message="Card Added" /> : null}
       </div>
     </div>
   );
